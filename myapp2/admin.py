041d394fbc78acf732f1934875575caabcbe0cde
Deniska1644 <deniskasupamida@gmail.com>
2023-12-11 14:19:37 +0300

    Home work 5

--- myapp2/admin.py
+++ myapp2/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import User, Product, Order
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'email', 'phone_number', 'date_registration']
+    ordering = ['-pk']
+    search_fields = ['name', 'email', 'phone_number']
+    search_help_text = 'Поиск по имени, почте, номеру телефона'
+    fields = ['name', 'email', 'date_registration', 'phone_number']
+    readonly_fields = ['name', 'phone_number', 'date_registration']
+
+
+@admin.action(description='вайп цен')
+def reset_price(modeladmin, request, queryset):
+    queryset.update(price=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity_product']
+    # fields = ['name', 'description', 'category', 'date_added','rating']
+    readonly_fields = ['quantity_product']
+    actions = [reset_price]
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Категория товара и его подробное описание',
+                'fields': ['description'],
+            },
+        ),
+        (
+            'Цена',
+            {
+                'fields': ['price', 'quantity_product'],
+            }
+        )]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
--- myapp2/models.py
+++ myapp2/models.py
@@ -9,6 +9,10 @@ class User(models.Model):
     address = models.CharField(max_length=300)
     date_registration = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return (f'name : {self.name}, email: {self.email}, phone number: {self.phone_number},'
+                f'adres: {self.address}, date registration: {self.date_registration}')
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -17,9 +21,17 @@ class Product(models.Model):
     quantity_product = models.IntegerField()
     image = models.ImageField(blank=True, upload_to='media/')
 
+    def __str__(self):
+        return (f'name: {self.name}, description: {self.description},price: {self.price},'
+                f'quantity: {self.quantity_product}')
+
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return (f'customer: {self.customer}, product: {self.products}, total price: {self.total_price},'
+                f'date order: {self.date_ordered}')
